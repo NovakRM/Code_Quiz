2da37ab7ae04f5949dff1e74f8dbb396868a512e
osteophagy <mnovakahs@gmail.com>
2020-12-14 19:40:38 -0500

    got timer to work, pseudocoded out a to-do list

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,19 +1,36 @@
 //initial page//
-const startScreen = document.querySelector("#start-screen") // initial screen
-const startButton = document.querySelector("#start") // start button, initializes timer and changes screen to questions?
+let startScreen = document.querySelector("#start-screen") // initial screen
+let startButton = document.querySelector("#start") // start button, initializes timer and changes screen to questions?
+let counterEl = document.querySelector("#time") //timer element
 
 startButton.addEventListener('click', callQuestions)
 
 function callQuestions(){
   startScreen.className = "hide" //changing the classname
   quizContainer.className = "show" //changing the classname
+  counterEl.textContent = 60
+  setTime()
+  console.log("start")
+}
+
+let timeLeft // global variable
+
+function setTime(){
+    timeLeft = setInterval(function(){
+        counterEl.textContent--;
+
+        if (parseInt(counterEl.textContent) === 0){
+            clearInterval(timeLeft) // can call clear interval from other areas of quiz logic
+            endQuiz()
+        }
+    }, 1000)
 }
 
 //quiz content//
-const quizContainer = document.querySelector("#questions") // dynamically updates with questions?
-const questionTitle = document.querySelector("#question-title") // h2 to populate with question itself?
-const questionOptions = document.querySelector("#choices") // radio buttons? 
-const quizQuestion = [
+let quizContainer = document.querySelector("#questions") // dynamically updates with questions?
+let questionTitle = document.querySelector("#question-title") // h2 to populate with question itself?
+let questionOptions = document.querySelector("#choices") // radio buttons? 
+let quizQuestion = [
     {
         question: "string",
         answers: {
@@ -27,19 +44,43 @@ const quizQuestion = [
 
 
 //quiz end//
-const endScreen = document.querySelector("#end-screen")
-const finalScore = document.querySelector("#final-score")
+let endScreen = document.querySelector("#end-screen")
+let finalScore = document.querySelector("#final-score")
+
 
 //logic start//
+
+//create eventListener for button [x]
+// activate timer countdown [x]
+// show questions [] working on it
+    // q arrays
+    // for loop pulling from array to make buttons and show questions
+    // event listener for each answer button
+    // add pt for correct answer
+        //set/get item from local storage
+    // wrong answer subtracts time (might need to just leave this out until the end)
+    // loop through questions one at a time
+    // keep time decrementing 
+    //final question brings user to score screen
+
+//quiz end
+    // value of locally stored number displayed as score
+    // input field for initials
+        // store initials and score to local
+        // link to high scores page
+// high scores page
+    // create an element displaying initials and scores
+
+
 function buildQuiz(){
-     const output = [] //variable for HTML generation
+     let output = [] //variable for HTML generation
     
      quizQuestions.forEach((currentQuestion, qNumber) => {//foreach loop for questions object array. 
-         const answers = [] //array containing possible answers
+         let answers = [] //array containing possible answers
          for (letter in currentQuestion.answers){ // loop to add in button
              answers.push( // push is adding these items to the answers array and slapping down a button per item.
  				"<label>"
- 					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+ 					+ '<input type="button" name="question'+i+'" value="'+letter+'">'
  					+ letter + ": "
 					+ currentQuestion[i].answers[letter]
 				+ "</label>"
@@ -49,3 +90,4 @@ function buildQuiz(){
  }
 
  function showResults(){}
+ 
