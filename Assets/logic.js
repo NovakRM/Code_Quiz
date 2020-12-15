4e072cbddb5f138852e4060c3de39a7f5ca57921
osteophagy <mnovakahs@gmail.com>
2020-12-14 20:58:56 -0500

    changing approach from objects to arrays. mentioned to classmate if else on eventlisteners to categorize right and wrong answers might make code more dry

--- Assets/logic.js
+++ Assets/logic.js
@@ -3,9 +3,9 @@ let startScreen = document.querySelector("#start-screen") // initial screen
 let startButton = document.querySelector("#start") // start button, initializes timer and changes screen to questions?
 let counterEl = document.querySelector("#time") //timer element
 
-startButton.addEventListener('click', callQuestions)
+startButton.addEventListener('click', startQuiz)
 
-function callQuestions(){
+function startQuiz(){
   startScreen.className = "hide" //changing the classname
   quizContainer.className = "show" //changing the classname
   counterEl.textContent = 60
@@ -47,47 +47,65 @@ let quizQuestion = [
 let endScreen = document.querySelector("#end-screen")
 let finalScore = document.querySelector("#final-score")
 
+function endQuiz(){
+    quizContainer.className = "hide" //changing the classname
+    endScreen.className = "show" //changing the classname
+}
+
+//high score page//
+let initialsContainer = document.querySelector("#initials")
 
 //logic start//
 
-//create eventListener for button [x]
-// activate timer countdown [x]
-// show questions [] working on it
-    // q arrays
-    // for loop pulling from array to make buttons and show questions
-    // event listener for each answer button
-    // add pt for correct answer
-        //set/get item from local storage
-    // wrong answer subtracts time (might need to just leave this out until the end)
-    // loop through questions one at a time
-    // keep time decrementing 
-    //final question brings user to score screen
-
-//quiz end
-    // value of locally stored number displayed as score
-    // input field for initials
-        // store initials and score to local
-        // link to high scores page
-// high scores page
-    // create an element displaying initials and scores
-
-
-function buildQuiz(){
-     let output = [] //variable for HTML generation
+//make a for loop that adds buttons and call it on each array of questions
+//each question's going to be a miniature function, object doesnt seem in our scope?? idk or stick with object i just dont get it as much as i do for loops and arrays
     
-     quizQuestions.forEach((currentQuestion, qNumber) => {//foreach loop for questions object array. 
-         let answers = [] //array containing possible answers
-         for (letter in currentQuestion.answers){ // loop to add in button
-             answers.push( // push is adding these items to the answers array and slapping down a button per item.
- 				"<label>"
- 					+ '<input type="button" name="question'+i+'" value="'+letter+'">'
- 					+ letter + ": "
-					+ currentQuestion[i].answers[letter]
-				+ "</label>"
-           );
+//Talked with a classmate about using arrays and for loops, for loop to call in question functions. 
+//Mentioned I learned how to do for loop eventlisteners to make code more dry. Pasted in some code from my own experimenting as a reminder
+
+    function giveButtons() {
+        for (var i = 0; i < catTypes.length; i++) { // creating a for loop that trucks thru the array catTypes
+            var btn = document.createElement("button") // and generating a button
+            var txt = document.createTextNode(catTypes[i]) // and then telling it to enter values from the array as text
+            btn.setAttribute("class","CatButtons")
+            btn.appendChild(txt) // appending text node to button as a child
+            buttonEl.appendChild(btn) // appending the buttons to the button element
+          }
+    
+          //next: create event listener loop for on click ( element.addEventListener("event") )
+    
+          var CatButtons = document.getElementsByClassName("CatButtons")
+          
+          for (var i = 0; i < CatButtons.length; i++) { 
+            if (CatButtons[i].innerHTML === "sphynx"){ // .innerHTML is checking the text attribute inside the element
+                CatButtons[i].addEventListener("click", function(){
+                    alert("Sphynx!")
+                }) 
+                //using functions, when implementing arguments, rule of thumb is you add strings unless you're adding a vairable representing a string
+            }else{
+                CatButtons[i].addEventListener("click", function(){
+                    console.log("Normal Cat!")   
+            }
+         )}
         }
-     })
- }
+
+
+// function buildQuiz(){
+//      let output = [] //variable for HTML generation
+    
+//      quizQuestions.forEach((currentQuestion, qNumber) => {//foreach loop for questions object array. 
+//          let answers = [] //array containing possible answers
+//          for (letter in currentQuestion.answers){ // loop to add in button
+//              answers.push( // push is adding these items to the answers array and slapping down a button per item.
+//  				"<label>"
+//  					+ '<input type="button" name="question'+i+'" value="'+letter+'">'
+//  					+ letter + ": "
+// 					+ currentQuestion[i].answers[letter]
+// 				+ "</label>"
+//            );
+//         }
+//      })
+//  }
 
  function showResults(){}
  
