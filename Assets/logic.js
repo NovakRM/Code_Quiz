81dbc20e0a4f5f8c8ab0792a575b110f9fa32ef7
osteophagy <mnovakahs@gmail.com>
2020-12-17 21:39:13 -0500

    got questions to pull from object containing arrays

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,19 +1,24 @@
 //initial page//
+
+//global variables//
 let startScreen = document.querySelector("#start-screen") // initial screen
 let startButton = document.querySelector("#start") // start button, initializes timer and changes screen to questions?
 let counterEl = document.querySelector("#time") //timer element
 
-startButton.addEventListener('click', startQuiz)
+//Score
+var score = 0;
 
-function startQuiz(){
+startButton.addEventListener('click', startQuiz) //event listener for click, calls startquiz
+
+function startQuiz(){ 
   startScreen.className = "hide" //changing the classname
   quizContainer.className = "show" //changing the classname
   counterEl.textContent = 60
   setTime()
-  giveQOne()
+  nextQuestion(questionArray[0]) // change questions calling function? 
 }
 
-let timeLeft // global variable
+let timeLeft // global variable for countdown
 
 function setTime(){
     timeLeft = setInterval(function(){
@@ -28,79 +33,98 @@ function setTime(){
 
 //quiz content//
 let quizContainer = document.querySelector("#questions") // dynamically updates with questions?
-var questionTitle = document.querySelector("#question-title") // h2 to populate with question itself?
+let questionTitleEl = document.querySelector("#question-title") // h2 to populate with question itself?
 let questionOptions = document.querySelector("#choices") // button styling already written
+
+//logic start//
+
+//make a for loop that adds buttons and call it on each array of questions
+
+function giveButtons(give) {
+    for (var i = 0; i < give.length; i++) {
+       var btn = document.createElement("button")
+       var txt = document.createTextNode(give[i])
+       btn.appendChild(txt)
+       btn.id = [i] //assigning buttons to div
+       btn.classList.add("answerButton")  //string naming object class
+       questionOptions.appendChild(btn)
+    }
+}
+
+//let currentQuestion //global variable for current question being stored
+
+
+let questionArray = [
+    {
+        questionTitle: "Question 1",
+        answerOptions: [ "a","b","c","d"],
+        correctAnswer: 3 //index of correct answer, D
+    },
+    {
+        questionTitle: "Question 2",
+        answerOptions: [ "a","b","c","d"],
+        correctAnswer: 1 //index of correct answer, B
+    },
+    {
+        questionTitle: "Question 3",
+        answerOptions: [ "a","b","c","d"],
+        correctAnswer: 1 //index of correct answer, B
+    },
+    {
+        questionTitle: "Question 4",
+        answerOptions: [ "a","b","c","d"],
+        correctAnswer: 2 //index of correct answer, C
+    }
+]
+
+let questionIndexPos = 0 // telling the browser where we start in the index above, it's kind of like how you have to call a function. You're telling the thing what to do
+
+document.addEventListener("click", function(event){
+    let classArr = Array.from(event.target.classList) // creates array from argument
+    let currentQuestion = questionArray[questionIndexPos]
+    if (classArr.includes("answerButton")){
+        if(currentQuestion.answerOptions.indexOf(event.target.textContent)===currentQuestion.correctAnswer){  //find the index of the textcontent of whatever we clicked on in answer options
+            correct()
+        }else{
+            incorrect()
+        }   
+        questionIndexPos ++ // increment index and move forward one
+            nextQuestion(questionArray[questionIndexPos])// calls next question, pulls from array
+    }
+})
+
+
+function nextQuestion(thisQuestion){ 
+    if(thisQuestion !== undefined){ // if this has a value (basically we're telling the browser only to run this logic if it's still got the array to pull from)
+        questionTitleEl.textContent = thisQuestion.questionTitle
+        questionOptions.textContent = ""
+        giveButtons(thisQuestion.answerOptions) //spawning buttons
+    } else {
+        clearInterval(timeLeft)
+        endQuiz()
+    }  
+}
+
+function incorrect() {
+    alert("wrong!") // get something less ugly than an alert firing
+    //subtract time
+}
+
+function correct() {
+    alert("right!")
+    //add point
+}
 
 //quiz end//
 let endScreen = document.querySelector("#end-screen")
 let finalScore = document.querySelector("#final-score")
 
+function showScore() {
+    finalScore.textContent = localStorage.getItem("score")
+}
+
 function endQuiz(){
     quizContainer.className = "hide" //changing the classname
     endScreen.className = "show" //changing the classname
+    showScore()
 }
-
-//high score page//
-let initialsContainer = document.querySelector("#initials")
-
-//logic start//
-
-//make a for loop that adds buttons and call it on each array of questions
-//each question's going to be a miniature function, object doesnt seem in our scope?? idk or stick with object i just dont get it as much as i do for loops and arrays
-    
-//Talked with a classmate about using arrays and for loops, for loop to call in question functions. 
-//Mentioned I learned how to do for loop eventlisteners to make code more dry. Pasted in some code from my own experimenting as a reminder
-
-function giveButtons(give) {
-    for (var i = 0; i < give.length; i++) {
-       var btn = document.createElement("button");
-       var txt = document.createTextNode(give[i]);
-       btn.appendChild(txt);
-       btn.id = give[i];
-       questionOptions.appendChild(btn);
-    }
-}
-
-function incorrect() {
-    alert("wrong!")
-}
-
-function correct() {
-    alert("right!")
-}
-
-function giveQOne(){
-    questionTitle.textContent = "string"
-    let questionOne = ["A", "B", "C", "D"] //placeholders, actual qs to be written later
-    giveButtons(questionOne) // spawning buttons in
-    let q1c1 = document.querySelector (questionOne[0]) // telling to delegate to buttons from array
-    let q1c2 = document.querySelector (questionOne[1])
-    let q1c3 = document.querySelector (questionOne[2])
-    let q1c4 = document.querySelector (questionOne[3])
-    q1c1.addEventListener("click", incorrect)
-    q1c2.addEventListener("click", incorrect)
-    q1c3.addEventListener("click", correct)
-    q1c4.addEventListener("click", incorrect)
-    }
-
-
-    //next: create event listener loop for on click ( element.addEventListener("event") )   
-
-//     function assignButtons(){ // looping eventListener? is it less repetition this way even if it's long?
-//     for (var i = 0; i < give.length; i++) { 
-//         giveButtons[i] = document.getElementById(give[i])
-//     if (give[i].innerHTML === "correct answer??"){ // change from innerhtml youre looking for something else, need to remeber
-//         CatButtons[i].addEventListener("click", function(){
-//             //add point
-//             //go to next question
-//         }) 
-//         //using functions, when implementing arguments, rule of thumb is you add strings unless you're adding a vairable representing a string
-//     }else{
-//         CatButtons[i].addEventListener("click", function(){
-//             //subtract time
-//             //go to next question
-//       }
-//     )}
-// }
-
-// function showResults(){}
